4e600e3fd245151d05c3d59c27d66f4317ca2692
hoseasandstrom <Lakyn4ever>
2016-06-30 11:28:05 -0400

    added controller functions

--- src/main/java/com/theitronyard/WebSocketController.java
+++ src/main/java/com/theitronyard/WebSocketController.java
@@ -1,5 +1,10 @@
 package com.theitronyard;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -7,5 +12,17 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class WebSocketController {
+    static SimpMessagingTemplate messenger;
 
+    @Autowired
+    public WebSocketController(SimpMessagingTemplate messenger) {
+        this.messenger = messenger;
+
+    }
+
+    @MessageMapping("/move")
+    @SendTo("/move")
+    public Message move(Message message) {
+        return message;
+    }
 }
